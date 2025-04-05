2b34e092884addeefabbc14e649657aa924cb799
joel <joel.edwardson1@gmail.com>
2025-04-05 15:47:58 +0100

    start adding treesitter navigation hotkeys

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1037,21 +1037,76 @@ require('lazy').setup {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup {
+
+        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+          enable = true,
+          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          --  If you are experiencing weird indenting issues, add the language to
+          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+      }
+
+      -- Treesitter nav utils
+      local function get_current_ts_node()
+        local ts_utils = require 'nvim-treesitter.ts_utils'
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local parser_ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+        if not parser_ok or not parser then
+          vim.notify('Treesitter parser not active for this buffer', vim.log.levels.INFO)
+          return nil
+        end
+
+        local node = ts_utils.get_node_at_cursor()
+
+        return node
+      end
+
+      local function jump_to_node(node)
+        if not node then
+          return
+        end
+        vim.notify 'going to node!'
+        local row, col = node:range()
+        vim.api.nvim_win_set_cursor(0, { row + 1, col })
+      end
+
+      vim.keymap.set('n', ']j', function()
+        local cur_node = get_current_ts_node()
+        if not cur_node then
+          print 'no current node!'
+          return
+        end
+        local next_node = cur_node:next_named_sibling()
+        if not next_node then
+          print 'no next node!'
+          return
+        end
+        print 'got next node'
+
+        jump_to_node(next_node)
+      end, { desc = 'Treesitter: Next sibling node' })
+
+      vim.keymap.set('n', '[j', function()
+        jump_to_node(get_current_ts_node():prev_named_sibling())
+      end, { desc = 'Treesitter: Previous sibling node' })
+
+      vim.keymap.set('n', ']t', function()
+        jump_to_node(get_current_ts_node():named_child(0))
+      end, { desc = 'Treesitter: Enter first child node' })
+
+      vim.keymap.set('n', '[t', function()
+        jump_to_node(get_current_ts_node():parent())
+      end, { desc = 'Treesitter: Go to parent node' })
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
@@ -1318,6 +1373,7 @@ require('lazy').setup {
   --     },
   --   },
   -- },
+  { 'mbbill/undotree' },
 
   { import = 'custom.plugins' },
 
@@ -1391,7 +1447,7 @@ vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99 -- start with all folds open
 vim.opt.foldlevelstart = 99 -- start with all folds open
-vim.keymap.set('n', 'zO', 'zczA', { desc = 'Open fold and enter insert' })
+vim.keymap.set('n', 'zO', 'zxzczA', { desc = 'Open fold and enter insert' })
 
 -- Window focus highlighting (NC = Non-Current/inactive windows)
 vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
