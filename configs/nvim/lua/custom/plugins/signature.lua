355769d849f3132b86ef60080167675d667e5354
joel <joel.edwardson1@gmail.com>
2025-05-27 09:47:58 +0100

    add outline and trouble

--- configs/nvim/lua/custom/plugins/signature.lua
+++ configs/nvim/lua/custom/plugins/signature.lua
@@ -8,6 +8,8 @@ return {
     },
     floating_window_above_cur_line = false,
     toggle_key = '<C-x>',
+    hint_enable = false,
+    floating_window = false,
   },
   config = function(_, opts)
     require('lsp_signature').setup(opts)
