451b28e506ca26a57cf07f1cb61f83f7d74f19f4
joel <joel.edwardson1@gmail.com>
2025-08-20 16:50:12 +0100

    added fish functions for viewing and editing bw items

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -17,7 +17,7 @@ end
 
 fish_config theme choose "Dracula Official"
 
-function setup-bw
+function bw-setup
     if ! command -v jq >/dev/null
         echo "jq not found!"
         return 1
@@ -40,6 +40,72 @@ function setup-bw
     end
 end
 
+function bw-view
+    # get all items from bitwarden
+    set bw_items $(bw list items)
+
+    # pass ID (hidden as 1st col) to fzf with details
+    set selected_id $(echo $bw_items | jq -r '.[] | .id + " " + .name + " " + .login.username' | fzf --with-nth 2.. | awk '{print $1}')
+
+    # check user selected an item
+    if test -z $selected_id
+        echo "no item selected!"
+        exit 1
+    end
+
+    # filter items based on selected ID and convert to yaml
+    echo $bw_items | jq -r ".[] | select(.id == \"$selected_id\")" | yq -p=json
+end
+
+function bw-edit
+    # get all items from bitwarden
+    set bw_items $(bw list items)
+
+    if test -z bw_items
+        return 1
+    end
+
+    # pass ID (hidden as 1st col) to fzf with details
+    set selected_id $(echo $bw_items | jq -r '.[] | .id + " " + .name + " " + .login.username' | fzf --with-nth 2.. | awk '{print $1}')
+
+    # check user selected an item
+    if test -z $selected_id
+        echo "no item selected!"
+        return 1
+    end
+    echo "retrieved ID $selected_id"
+
+    # create temp file
+    set tmpfile (mktemp)
+
+    # write JSON to temp file
+    echo $bw_items | jq -r ".[] | select(.id == \"$selected_id\")" >$tmpfile
+
+    # check file not empty
+    if test -z "$(cat -A $tmpfile)"
+        echo "error, json is empty!"
+        return 1
+    end
+
+    # open in editor (this blocks until editor closes)
+    $EDITOR $tmpfile
+
+    if test "$(cat $tmpfile)" = "$item_json"
+        echo "no changes made, exiting..."
+        rm -f $tmpfile
+        return 0
+    end
+
+    # update bitwarden with edited JSON
+    echo "sending updated JSON to bitwarden..."
+    cat $tmpfile | bw encode | bw edit item $selected_id
+
+    # cleanup
+    rm -f $tmpfile
+
+    echo "Item $selected_id updated successfully"
+end
+
 # Essential tmux aliases
 alias ta='tmux attach' # Attach to a session
 alias ts='tmux new-session -s' # Start new session with name
@@ -47,6 +113,7 @@ alias tl='tmux list-sessions' # List all sessions
 
 # Git alises
 alias g='git'
+alias lg='lazygit'
 alias ga='git add'
 alias gcm='git commit -m'
 alias gp='git push'
