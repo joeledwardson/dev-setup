12fb5561ae41e17c725549832fbb0701edbc351b
joel <joel.edwardson1@gmail.com>
2025-09-05 13:06:34 +0100

    re-add remember to tuigreet

--- modules/nixos-base.nix
+++ modules/nixos-base.nix
@@ -314,7 +314,8 @@ in {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command =
+          "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
         user = "greeter";
       };
     };
