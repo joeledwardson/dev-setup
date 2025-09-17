3e149bcb24514b7c230ead53b9b8c0ca13510fca
joel <joel.edwardson1@gmail.com>
2025-09-17 11:20:46 +0100

    add window expand menu

--- configs/hypr/scripts/window-menu.sh
+++ configs/hypr/scripts/window-menu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-choice=$(echo -e "expand-horizontally\nexpand-vertically" | fuzzel --prompt="what do you want to do?" --dmenu)
+choice=$(echo -e "expand-upwards\nexpand-across" | fuzzel --prompt="what do you want to do?" --dmenu)
 if [[ -z "$choice" ]]; then
     notify-send "cancelled"
     exit
@@ -9,9 +9,11 @@ fi
 # get active window address
 active_address=$(hyprctl activewindow -j | jq -r '.address')
 
-# get start and end x of window
-active_x=$(hyprctl activewindow -j | jq -r '.at[0]')
+# get coords
+start_x=$(hyprctl activewindow -j | jq -r '.at[0]')
 end_x=$(hyprctl activewindow -j | jq -r '.at[0] + .size[0]')
+start_y=$(hyprctl activewindow -j | jq -r '.at[1]')
+end_y=$(hyprctl activewindow -j | jq -r '.at[1] + .size[1]')
 
 # get workspace address
 workspace_id=$(hyprctl activewindow -j | jq -r '.workspace.id')
@@ -20,35 +22,36 @@ if [[ -z "$workspace_id" || -z "$active_address" ]]; then
     exit 1
 fi
 
-echo "got workspace ID to be: $workspace_id, address to be $active_address and x: $active_x => $end_x"
+echo "got workspace ID to be: $workspace_id, address to be $active_address"
+echo "got x: $start_x => $end_x, y: $start_y => $end_y"
 
-if [[ "$choice" == 'expand-horizontally' ]]; then
-    # get range x values for windows on same workspace
-    min_x=$(hyprctl clients -j | jq -r ".[] | select(.workspace.id == $workspace_id) | .at[0]" | sort -n | head -n 1)
-    max_x=$(hyprctl clients -j | jq -r ".[] | select(.workspace.id == $workspace_id) | .at[0]" | sort -n --reverse | head -n 1)
-    echo "got min x: $min_x and max x: $max_x"
 
-    # check values are valid
-    if [[ -z "$min_x" || -z "$max_x" ]]; then
-        notify-send "failed to get min/max x"
-        exit 1
-    fi
+# create jq conditions to get other windows that overlap with the active window's x position
+conditions=$(printf '.address != "%s" and .workspace.id == %s' "$active_address" "$workspace_id")
 
-    # create jq conditions to get other windows that overlap with the active window's x position
-    conditions=$(printf '.address != "%s" and .workspace.id == %s and (.at[0] < %s or (.at[0] + .size[0]) > %s)' \
-        "$active_address" "$workspace_id" "$end_x" "$active_x")
-    echo "got conditions: $conditions"
-    
-    # push windwos to special workspace and back again (SHOULD) mean the desired active window swallows up the wanted space
-    for window_address in $(hyprctl clients -j | jq -r ".[] | select($conditions and .at[0] < $max_x) | .address"); do
-        # Send window to special workspace temporarily
-      hyprctl dispatch focuswindow "address:$window_address"
-      hyprctl dispatch movetoworkspace special:magic
-      # Bring it back to current workspace
-      hyprctl dispatch movetoworkspace $workspace_id
-    done
-
-    echo "going back to address: $active_address"
-    hyprctl dispatch focuswindow "address:$active_address"
-
+if [[ "$choice" == "expand-upwards" ]]; then
+    # for vertical, filter to windows that overlap with x coordinates
+    conditions+=" and (.at[0] < $end_x and (.at[0] + .size[0]) > $start_x)"
+else
+    # horizontal is with overlapping y
+    conditions+=" and (.at[1] < $end_y and (.at[1] + .size[1]) > $start_y)"
 fi
+
+
+echo "got conditions: $conditions"
+
+# push windwos to special workspace and back again (SHOULD) mean the desired active window swallows up the wanted space
+echo "processing windows...."
+for window_address in $(hyprctl clients -j | jq -r ".[] | select($conditions) | .address"); do
+    echo "handling window $window_address ....."
+    # Send window to special workspace temporarily
+    hyprctl dispatch focuswindow "address:$window_address"
+    hyprctl dispatch movetoworkspace special:magic
+    # Bring it back to current workspace
+    hyprctl dispatch movetoworkspace $workspace_id
+done
+echo "done processing windows...."
+
+echo "going back to address: $active_address"
+hyprctl dispatch focuswindow "address:$active_address"
+
