d22bacb141a7b60fa0e8bc42eb0bdb73f5f8c122
joel <joel.edwardson1@gmail.com>
2025-10-19 12:15:11 +0100

    add vue, configure dadbod and tweak tmux

--- configs/nvim/lua/custom/plugins/dadbod.lua
+++ configs/nvim/lua/custom/plugins/dadbod.lua
@@ -1,33 +1,36 @@
 return {
   {
-    "kristijanhusak/vim-dadbod-ui",
+    'kristijanhusak/vim-dadbod-ui',
     dependencies = {
-      { "tpope/vim-dotenv", lazy = true },
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+      { 'tpope/vim-dotenv', lazy = true },
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
     },
     cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
     },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_winwidth = 30
       vim.g.db_ui_show_help = 0
       vim.g.db_ui_use_nvim_notify = 1
-      vim.g.db_ui_win_position = "left"
+      vim.g.db_ui_win_position = 'left'
 
-      require("which-key").register {
-        ["<leader>D"] = {
-          name = "󰆼 Db Tools",
-          u = { "<cmd>DBUIToggle<cr>", " DB UI Toggle" },
-          f = { "<cmd>DBUIFindBuffer<cr>", " DB UI Find buffer" },
-          r = { "<cmd>DBUIRenameBuffer<cr>", " DB UI Rename buffer" },
-          l = { "<cmd>DBUILastQueryInfo<cr>", " DB UI Last query infos" },
+      require('which-key').register {
+        ['<leader>D'] = {
+          name = '󰆼 Db Tools',
+          u = { '<cmd>DBUIToggle<cr>', ' DB UI Toggle' },
+          f = { '<cmd>DBUIFindBuffer<cr>', ' DB UI Find buffer' },
+          r = { '<cmd>DBUIRenameBuffer<cr>', ' DB UI Rename buffer' },
+          l = { '<cmd>DBUILastQueryInfo<cr>', ' DB UI Last query infos' },
         },
       }
+
+      -- Save query keybinding (works in sql buffers)
+      vim.keymap.set('n', '<leader>W', '<Plug>(DBUI_SaveQuery)', { desc = 'Save query permanently' })
     end,
   },
 }
